9e75a41bedb3af59aca78d832f9cdfcc843baa0e
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2025-01-27 14:29:34 +0100

    Proyecto perfil Postgre creado.

---
+++ ejemploPerfilesPostgre/src/main/java/com/example/ejemploPerfilesPostgre/EjemploPerfilesPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemploPerfilesPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploPerfilesPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploPerfilesPostgreApplication.class, args);
+	}
+
+}
---
+++ ejemploPerfilesPostgre/src/test/java/com/example/ejemploPerfilesPostgre/EjemploPerfilesPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemploPerfilesPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploPerfilesPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
